9e48943bec3026166b566a86e78e43d3f797de70
mahaahmedkassem <mahakassem22@gmail.com>
2023-11-13 18:20:14 +0200

    git config –global –list

---
+++ routes/web.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('test', function () {
+    return 'welcome to my first route';
+});
+
+// Route::get('user/{name}/{age}', function ($name, $age) {
+//     return 'the username is :' . $name . ' and age is ' . $age;
+// });
+
+// Route::get('user/{name}/{age?}', function ($name, $age=0){
+// if($age==0) {
+//     return 'the username is :' . $name ;
+// }
+// else{
+//     return 'the username is :' . $name . ' and age is ' . $age;
+// }}
+// );
+
+Route::get ('user/{name}/{age?}',function($name,$age=0){
+    $msg = 'the username is : ' . $name;
+    if($age > 0){
+        $msg .= ' and age is: ' . $age ;
+    }
+    return $msg ;
+})->whereIn('name',['adam','ahmed']);
+
+// ->where(['age'=>'[0-9]+' ,'name'=>'[a-zA-z0-9]+']);
+
+Route::prefix('product')->group(function(){
+    Route::get('/',function(){
+        return'producy home page';
+    });
+
+    Route::get('laptop',function(){
+        return'laptop page';
+    });
+    Route::get('camera',function(){
+        return'camera page';
+    });
+    Route::get('phone',function(){
+        return'phone page';
+    });
+
+}
+);
+

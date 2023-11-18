6671ae3ea069f8ff5fb190fddda7afdaf9010c8e
mahaahmedkassem <mahakassem22@gmail.com>
2023-11-18 22:16:31 +0200

    $update

--- app/Http/Controllers/CarlistController.php
+++ app/Http/Controllers/CarlistController.php
@@ -7,11 +7,9 @@ use Illuminate\Http\Request;
 class CarlistController extends Controller
 {
     public function test2(Request $request){
-        $title = $_POST['title'];
-        $price =  $_POST['price'];
-        return view ("addcar ")
-        ->with('title', $title)
-                    ->with('price', $price);;
+       
+        return view ("addcar ");
+       
         
     }
 }
